c77bc895cda81210be58f6010f1d33a0d7b0f728
victorhabila <48987601+victorhabila@users.noreply.github.com>
2024-10-02 20:48:21 +0200

    Initail project

---
+++ homesawpbackend/homesawpbackend/src/main/java/com/homeSwap/homesawpbackend/homesawpbackend/HomesawpbackendApplication.java
@@ -0,0 +1,13 @@
+package com.homeSwap.homesawpbackend.homesawpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomesawpbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomesawpbackendApplication.class, args);
+	}
+
+}
---
+++ homesawpbackend/homesawpbackend/src/test/java/com/homeSwap/homesawpbackend/homesawpbackend/HomesawpbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeSwap.homesawpbackend.homesawpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomesawpbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
