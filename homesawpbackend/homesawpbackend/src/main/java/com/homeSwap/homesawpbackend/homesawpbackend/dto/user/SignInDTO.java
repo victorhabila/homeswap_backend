31527ddd974a7d88ca57f7f41cce97d6e2e8baf6
victorhabila <48987601+victorhabila@users.noreply.github.com>
2024-10-03 20:12:29 +0200

    SignInDto and SignIn response dto

---
+++ homesawpbackend/homesawpbackend/src/main/java/com/homeSwap/homesawpbackend/homesawpbackend/dto/user/SignInDTO.java
@@ -0,0 +1,26 @@
+package com.homeSwap.homesawpbackend.homesawpbackend;
+
+private String email;
+private String password;
+
+public SignInDTO(String email, String password) {
+        this.email = email;
+        this.password = password;
+        }
+
+public String getEmail() {
+        return email;
+        }
+
+public void setEmail(String email) {
+        this.email = email;
+        }
+
+public String getPassword() {
+        return password;
+        }
+
+public void setPassword(String password) {
+        this.password = password;
+        }
+        }
---
+++ homesawpbackend/homesawpbackend/src/main/java/com/homeSwap/homesawpbackend/homesawpbackend/dto/user/SignInResponseDTO.java
@@ -0,0 +1,29 @@
+package com.homeSwap.homesawpbackend.homesawpbackend;
+
+
+public class SignInResponseDTO {
+
+    private String status;
+    private String token;
+
+    public SignInResponseDTO(String status, String token) {
+        this.status = status;
+        this.token = token;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
