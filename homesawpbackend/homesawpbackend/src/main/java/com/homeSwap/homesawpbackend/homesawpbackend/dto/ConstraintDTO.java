70f23dbb6d3697df95a3e7ca55a8f863c04fdbf1
victorhabila <48987601+victorhabila@users.noreply.github.com>
2024-10-03 20:30:45 +0200

    housing and booking dto classes

---
+++ homesawpbackend/homesawpbackend/src/main/java/com/homeSwap/homesawpbackend/homesawpbackend/dto/ConstraintDTO.java
@@ -0,0 +1,64 @@
+package com.homeSwap.homesawpbackend.homesawpbackend;
+
+
+import com.homeSwap.homesawpbackend.homesawpbackend.model.Constraint;
+
+import java.util.Date;
+
+public class ConstraintDTO {
+
+    private Integer id;
+    private String constraint_text;
+    private Integer houseId;
+
+    public String getConstraint_text() {
+        return constraint_text;
+    }
+
+    public void setConstraint_text(String constraint_text) {
+        this.constraint_text = constraint_text;
+    }
+
+    public ConstraintDTO() {
+
+    }
+
+
+    public ConstraintDTO(String constraint, Integer houseId) {
+        this.constraint_text = constraint;
+        this.houseId = houseId;
+    }
+
+    public  ConstraintDTO(Constraint constraint)
+    {
+        this.setId(constraint.getId());
+        this.setConstraint(constraint.getConstraint());
+        this.setHouseId(constraint.getHouse().getId());
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getConstraint() {
+        return constraint_text;
+    }
+
+    public void setConstraint(String constraint) {
+        this.constraint_text = constraint;
+    }
+
+    public Integer getHouseId() {
+        return houseId;
+    }
+
+    public void setHouseId(Integer houseId) {
+        this.houseId = houseId;
+    }
+
+
+}
