f1d951e5fe4012a1dd9daaa4762b452761081d75
victorhabila <48987601+victorhabila@users.noreply.github.com>
2024-10-04 21:08:01 +0200

    user repository implementation

---
+++ homesawpbackend/homesawpbackend/src/main/java/com/homeSwap/homesawpbackend/homesawpbackend/repository/UserRepository.java
@@ -0,0 +1,25 @@
+package com.homeSwap.homesawpbackend.homesawpbackend;
+
+
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User findByEmail(String email);
+    @Modifying
+    @Transactional
+    @Query( value="UPDATE users u SET u.enabled = true WHERE u.id= :id", nativeQuery=true)
+    public void enable(@Param("id")Integer id);
+
+    @Query( value="SELECT * FROM users u WHERE u.verification_code = :verification_code", nativeQuery=true)
+    public User findByVerificationCode(@Param("verification_code")String code);
+
+
+}
