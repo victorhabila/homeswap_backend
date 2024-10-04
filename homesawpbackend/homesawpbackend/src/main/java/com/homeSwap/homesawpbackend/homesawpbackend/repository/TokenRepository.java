dbecc1fc4387a1ba8b82f195cf7afe0811293535
victorhabila <48987601+victorhabila@users.noreply.github.com>
2024-10-04 21:14:13 +0200

    Housing  and Token repository

---
+++ homesawpbackend/homesawpbackend/src/main/java/com/homeSwap/homesawpbackend/homesawpbackend/repository/TokenRepository.java
@@ -0,0 +1,14 @@
+package com.homeSwap.homesawpbackend.homesawpbackend;
+
+import com.homeSwap.homeswapbackend.model.AuthenticationToken;
+import com.homeSwap.homeswapbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TokenRepository extends JpaRepository<AuthenticationToken, Integer> {
+
+    AuthenticationToken findByUser(User user);
+}
+
+
